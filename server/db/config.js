f0b6d47ef14858da17ac9a7da2bb40918e462202
NirajBurnwal <burnwalniraj2009@gmail.com>
2022-12-26 13:52:38 +0530

    first commit

--- server/db/config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require ("mongoose")
-
-const URI = process.env.DATABASE
-
-mongoose.connect( URI, {
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=> console.log("connection start"))
-.catch((error)=>console.log(error.message))
